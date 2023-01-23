63e67b0d88de8e3447c2a8a023a0d5e5d180ab8d
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-23 22:54:33 +0000

    practice area under work

---
+++ src/components/area/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css"
+
+const Area =()=>{
+    return(
+        <div>
+              <h1>Contact Page</h1>
+        </div>
+    )
+}
+
+export default Area;
---
+++ src/components/area/practice-area.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css"
+
+const PracticeArea =()=>{
+    return( 
+         <div classname="practise-container">
+                 <div>
+
+                 </div>
+
+                 <div>
+                    
+                 </div>
+         </div>      
+    )
+}
+
+export default PracticeArea;
