075bd5b8ad23b8a6bbe5be4f119c77dfd5946480
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-24 01:56:19 +0000

    Practice page done

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,12 +3,14 @@ import "./styles.css";
 import Layout from "../../layout";
 import LandingHero from "./landing";
 import AboutUs from "../about/about-us";
+import PracticeArea from "../area/practice-area";
 
 const Home =()=>{
 return(
     <Layout>
         <LandingHero/>
         <AboutUs/>
+        <PracticeArea/>
     </Layout>
     )
 }
