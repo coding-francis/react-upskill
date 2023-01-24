8a976f4880dfd9f065b9ccdc27f3e8c3a9d20b75
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-24 17:55:55 +0000

    images added

--- src/components/attorney/attorney.js
+++ src/components/attorney/attorney.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./styles.css"
-
+import "./styles.css";
+import {FcLeft} from "react-icons/fc";
+import {FcRight} from "react-icons/fc";
 
 const AttorneysPage =()=>{
     return(
@@ -9,12 +10,11 @@ const AttorneysPage =()=>{
                <div id="card1">
                 <div id="text">
                      <h2>What says our <br/> happy Clients</h2>
-                     
-
+                     <FcLeft size={50}/> <FcRight size={50}/>                   
                 </div>
 
                 <div>
-
+                      
                 </div>
                </div>
                
--- src/components/attorney/index.js
+++ src/components/attorney/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles.css"
 import Layout from "../../layout";
+import AttorneysPage from "./attorney";
 
 const Attorneys =()=>{
     return(
           <Layout>
-
+                 <AttorneysPage/>
           </Layout>
     )
 }
