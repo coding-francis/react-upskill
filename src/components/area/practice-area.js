40675ba180a74373035f12d13a0ff37c94d43397
Kurtis1955 <kurtisbill2000@gmail.com>
2023-01-24 00:11:44 +0000

    still on

--- src/components/area/practice-area.js
+++ src/components/area/practice-area.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./styles.css"
-
+import business from "../../assets/images/business.png";
+import business2 from "../../assets/images/business2.png";
+import abuse from "../../assets/images/abuse.png";
+import landlords from "../../assets/images/landlords.png";
+import partnership from "../../assets/images/partnership.png";
+import realestate from "../../assets/images/realestate.png";
+ 
 const PracticeArea =()=>{
     return( 
          <div classname="practise-container">
@@ -9,7 +15,20 @@ const PracticeArea =()=>{
                  </div>
 
                  <div classname="cards">
-                            
+                         <div classname="mini-card1">
+                               <h3>BUSINESS LAW <img src={business}alt=""/></h3>
+                               <h3>PARTNERSHIP LAW <img src={partnership} alt="" /></h3>
+                         </div>
+
+                         <div classname="mini-card2">
+                               <h3>REAL ESTATE LAW <img src={realestate} alt="" /></h3>
+                               <h3>BUSINESS LAW <img src={business2} alt="" /></h3>
+                         </div>
+
+                         <div classname="mini-card3">
+                              <img src={landlords} alt="" />
+                              <img src={abuse} alt="" />
+                         </div>   
                  </div>
          </div>      
     )
